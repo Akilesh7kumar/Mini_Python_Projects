4d91bccb6370fc747cfeeab6f77bf5cf4885ce24
AkileshKumar <56359926+Akilesh7kumar@users.noreply.github.com>
2024-02-11 13:55:34 +0530

    Create Generate_qr_using_pyth.py
    Basic python code to generate QR code for a web site

---
+++ Generate_qr_using_pyth.py
@@ -0,0 +1,12 @@
+#pip install qrcode
+
+import qrcode 
+from PIL import Image
+
+url = 'https://github.com/Akilesh7kumar'
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4)
+
+qr.add_data(url)
+qr.make(fit=True)
+img=qr.make_image(fill_color='black',back_color='white')
+img.save("github.png")
